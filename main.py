38f6b30caad618edabc861915b61fc992e49ae5f
yunusustun18 <yunusustun18@gmail.com>
2019-06-29 05:56:23 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://yellowpages.com.tr/ara?q=ankara+cafe")
-soup = BeautifulSoup(r.content)
+imdb_url = "https://www.imdb.com/chart/top"
+
+r = requests.get(imdb_url)
+
+soup = BeautifulSoup(r.content, "html.parser")
+
+data = soup.find_all("table", {"class": "chart full-width"})
+
+data = (data[0].contents)[len(data[0].contents) - 2]
+
+data = data.find_all("tr")
+
+for i in data:
+    new_data = i.find_all("td", {"class": "titleColumn"})
+    veri = new_data[0].text
+    veri = veri.replace("\n", "")
+    print(veri, "\n-------------\n")
